d50bf6abcbed2a8b3bcb55068fbc71dbe32e7a0d
adr <andres@duarterengifo.com>
2025-06-16 16:54:46 -0400

    flake works

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A flake providing a Python development shell with uv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python314;
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            python
+            pkgs.uv
+          ];
+          shellHook = ''
+            echo "Python: $(python --version)"
+            echo "uv: $(uv --version)"
+          '';
+        };
+      }
+    );
+}
